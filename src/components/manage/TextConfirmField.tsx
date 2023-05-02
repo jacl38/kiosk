f27fabe2aca1b246d762f6e20e9d1b887c807d42
Jack Clark <jcl1131@wgu.edu>
2023-05-01 20:25:53 -0400

    Refactored TextConfirmField component

--- src/components/manage/TextConfirmField.tsx
+++ src/components/manage/TextConfirmField.tsx
@@ -3,35 +3,57 @@ import { ButtonHTMLAttributes, InputHTMLAttributes, useRef } from "react"
 
 const styles = {
 	outerContainer: tw(
-		`rounded-full overflow-hidden`,
-		`border-2 border-stone-400 dark:border-gray-700`,
-		`flex`,
+		`rounded-2xl overflow-hidden`,
+		`w-full h-11`,
+		`relative flex`,
+		`bg-stone-300 dark:bg-gray-600`,
+		`border-2 border-stone-400 dark:border-gray-500`,
 		`transition-colors`,
+		`group`
 	),
 	textInput: tw(
-		`bg-stone-300 dark:bg-gray-600`,
-		`px-2 py-1`,
-		`transition-colors`
+		`w-full`,
+		`px-3`,
+		`bg-transparent`,
+		`outline-none`
+	),
+	label: tw(
+		`absolute`,
+		`left-3 -top-1`,
+		`text-sm`,
+		`opacity-50 group-hover:opacity-100`,
+		`transition-opacity`
 	),
 	button: tw(
-		`font-black`,
-		`w-8`,
+		`w-12 pb-2`,
+		`text-3xl font-black`,
+		`hover:bg-stone-400 dark:hover:bg-gray-500`,
+		`focus-visible:bg-stone-400 dark:focus-visible:bg-gray-500`,
+		`outline-none`,
+		`transition-colors`,
 	)
 }
 
-export default function TextConfirmField(props: {
+type TextConfirmFieldProps = {
+	key?: string,
+	label?: string,
+	suffix?: string,
 	inputProps?: InputHTMLAttributes<HTMLInputElement>,
 	buttonProps?: ButtonHTMLAttributes<HTMLButtonElement>,
 	onSubmit?: (value: string) => void
-}) {
+}
+
+export default function TextConfirmField(props: TextConfirmFieldProps) {
 	const inputRef = useRef<HTMLInputElement>(null);
 
 	function submit() {
 		props.onSubmit?.(inputRef.current?.value ?? "");
 	}
 
-	return <form onSubmit={e => { e.preventDefault(); submit(); }} className={styles.outerContainer}>
-		<input ref={inputRef} {...props.inputProps} className={styles.textInput} />
-		<button onClick={e => { e.preventDefault(); submit(); }} {...props.buttonProps} className={styles.button}>&rsaquo;</button>
-	</form>
+	return <div className={styles.outerContainer} onSubmit={submit}>
+		<label htmlFor={`${props.key}-input`} className={styles.label}>{props.label}</label>
+		<input id={`${props.key}-input`} ref={inputRef} {...props.inputProps} className={styles.textInput} />
+		<span className="my-auto select-none font-semibold">{props.suffix}</span>
+		<button className={styles.button} onClick={submit}>&rsaquo;</button>
+	</div>
 }
