a12ef8dfc67dcb82eee34283bb8a170cf31a5ab0
Jack Clark <jcl1131@wgu.edu>
2023-05-09 13:10:15 -0400

    Added personal info to order checkout screen (name, phone, notes)

--- src/pages/kiosk/checkout.tsx
+++ src/pages/kiosk/checkout.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import useMenu from "@/hooks/useMenu";
 import useLocalOrder from "@/hooks/useLocalOrder";
 import CheckoutItem from "@/components/Kiosk/CheckoutItem";
+import commonStyles from "@/styles/common";
 
 const styles = {
 	heading: {
@@ -24,7 +25,27 @@ const styles = {
 	itemList: tw(
 		`flex flex-col`,
 		`p-8 gap-y-4`
-	)
+	),
+	miscInfo: {
+		container: tw(
+			`mb-12 px-8`,
+			`flex flex-col`,
+			`gap-y-8`
+		),
+		inputGrid: tw(
+			`grid grid-cols-2`,
+			`gap-4`
+		),
+		inputBox: tw(
+			`px-4 py-2`,
+			`w-full`,
+			`rounded-lg`,
+			`border-b-2 border-b-hotchocolate-200`
+		),
+		label: tw(
+			`text-lg font-semibold`
+		)
+	}
 }
 
 export default function Checkout() {
@@ -44,6 +65,24 @@ export default function Checkout() {
 		<ul className={styles.itemList}>
 			{menu.menu && order.current.parts.map(part => <CheckoutItem menu={menu.menu!} part={part} />)}
 		</ul>
+
+		<div className={styles.miscInfo.container}>
+			<div className={styles.miscInfo.inputGrid}>
+				<div>
+					<label htmlFor="order-name" className={styles.miscInfo.label}>* Name:</label>
+					<input id="order-name" type="text" className={styles.miscInfo.inputBox} />
+				</div>
+				<div>
+					<label htmlFor="order-phone" className={styles.miscInfo.label}>Phone number:</label>
+					<input id="order-phone" type="tel" className={styles.miscInfo.inputBox} />
+				</div>
+				<div className="col-span-2">
+					<label htmlFor="order-notes" className={styles.miscInfo.label}>Notes:</label>
+					<textarea id="order-notes" className={tw(styles.miscInfo.inputBox, "resize-none")} />
+				</div>
+			</div>
+			<button className={tw(commonStyles.order.button, `text-2xl mx-auto`)}>Place Order</button>
+		</div>
 
 		<FloatingButton
 			action={() => router.push("/kiosk/menu")}>
